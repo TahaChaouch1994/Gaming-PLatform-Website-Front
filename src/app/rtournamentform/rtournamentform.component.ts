7a5a850fed707142e40731d27e177bb7034c4f50
Taha Chaouch <tahachaouch@hotmail.fr>
2020-02-16 23:35:14 +0100

    demander création tournoi terminé

--- src/app/rtournamentform/rtournamentform.component.ts
+++ src/app/rtournamentform/rtournamentform.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {GameRegion} from './gameregion';
 
 @Component({
   selector: 'app-rtournamentform',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RtournamentformComponent implements OnInit {
 
-  constructor() { }
+
+  selectedDay: string = '';
+  selectedMap: string ='';
+  selectedFormat: string='';
+
+  //event handler for the select element's change event
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.selectedDay = event.target.value;
+  }
+
+  selectChangeHandlerMap (event: any) {
+    //update the ui
+    this.selectedMap = event.target.value;
+  }
+
+  selectChangeHandlerFormat (event: any) {
+    //update the ui
+    this.selectedFormat = event.target.value;
+  }
+
+  
+   message = this.http.get<any[]>('http://localhost:3005/tournament');
+  
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+  iduser=1;
+  gamename='lol';
+  type='';
+  tournamentname='';
+  startdate='';
+  starttime='';
+  gameregion=this.selectedDay;
+  gamemap = this.selectedMap;
+  gameformat = this.selectedFormat ;
+  entry ='';
+  entryfee='';
+  minteams='';
+  maxteams='';
+  createtype='';
+  
+
+
+
+  justClick() {
+   const nametest = 'hahah';
+
+    this.http.get<any>('http://localhost:3005/tournament/createtournament/'+this.iduser+'/'+this.gamename+'/'+this.type+'/'+this.tournamentname+'/'+this.startdate+'/'+this.starttime+'/'+this.selectedDay+'/'+this.selectedMap+'/'+this.selectedFormat+'/'+this.entry+'/'+this.entryfee+'/'+this.minteams+'/'+this.maxteams+'/'+this.createtype).subscribe();
+     
+    console.log("GAME REGION ISSSSS" + this.selectedDay);
+  
+  }
+
+
+
+  
+
+
+
+
 
 }

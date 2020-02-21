cb55b2f0cdbe9d076a8b974f24a7ffc88cbe2d30
Samer <samer.belgacem@esprit.tn>
2020-02-21 16:39:51 +0100

    showing user merchs for everyone.

--- src/app/show-profile/show-profile.component.ts
+++ src/app/show-profile/show-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserApiService } from '../services/user-api.service';
+import { UserMerch } from '../models/user-merch';
+import { UsermerchService } from '../services/usermerch.service';
 
 @Component({
   selector: 'app-show-profile',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowProfileComponent implements OnInit {
 
-  constructor() { }
+  listMerch;
+  user: User;
+  
+  constructor(
+    public userApi : UserApiService,
+    public merchApi : UsermerchService,
+  ) { }
 
   ngOnInit() {
+    this.user = this.userApi.getLoggedInUser();
+    this.merchApi.getUserMerch(this.user.id_user).subscribe(response => {
+      this.listMerch = response;
+    });
   }
 
 }
